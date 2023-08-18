8fc833dcc401bbcb14beba0b10b0a6b997c2dc9d
kangsworkspace <141600830+kangsworkspace@users.noreply.github.com>
2023-08-18 14:22:27 +0900

    Initial Commit

---
+++ MovieTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTable
+//
+//  Created by Kang on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
